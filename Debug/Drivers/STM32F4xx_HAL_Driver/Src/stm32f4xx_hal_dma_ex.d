1fc7cda1fa50096b362a9ad9e7d0f8fca05fef3f
Bertoncelj_HP <tine.bertoncelj@gmail.com>
2018-11-25 19:36:10 +0100

    Load Project from CubeMX for AC6ST_eclipse

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.d
@@ -0,0 +1,77 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Inc/stm32f4xx_hal_conf.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Inc/main.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Include/core_cm4.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Include/core_cmSimd.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Inc/stm32f4xx_hal_conf.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Inc/main.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/home/tine/Documents/Embedded/Stm32/STM32F4/ProjectAC6/STM32F4_CubeTest01/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
